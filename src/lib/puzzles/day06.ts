b315959fa209bd57bb0769ee1e0e9d5c77c3601a
Cameron Messinides <cameron.messinides@gmail.com>
2023-12-10 10:57:23 -0500

    Optimize and explain solution for Day 6, Puzzle 1

--- src/lib/puzzles/day06.ts
+++ src/lib/puzzles/day06.ts
@@ -11,20 +11,38 @@ type Race = {
 }
 
 function countWinningSpeeds(race: Race): number {
-	let count = 0;
-
+	/*
+	  The winning speeds in these races have a symmetric distribution,
+	  where the midpoint between 0 and the duration is always the optimal
+	  speed. So, we can calculate the total number of winning speeds by
+	  finding the midpoint, finding the minimum winning speed, and
+	  doubling the difference between the two (roughly - see caveats below)
+	*/
 	const midpoint = Math.floor(race.duration / 2)
 
 	for (let s = 1; s <= midpoint; s++) {
 		let distance = calculateDistance(race.duration, s)
 
 		if (distance > race.recordDistance) {
-			count++
+			/*
+			  s is the minimum winning speed, so now the calculation:
+			  
+			  - (midpoint - s + 1) gives us the number of winning speeds
+			    between the minimum speed and midpoint (inclusive)
+			  - We double that value to give us the full count, _if_ the
+			    total number of available speeds is even.
+			  - In races where the total number of speeds is _odd_, we'll
+			    have counted the midpoint twice. To account for this, we
+			    subtract (race.duration % 2 ^ 1). race.duration % 2 is 1
+			    if the duration is odd, and 0 if even. The bitwise xor
+			    (^ 1) inverts that value, because an even duration implies
+			    an odd number of speeds, and vice versa.
+			*/
+			return (midpoint - s + 1) * 2 - (race.duration % 2 ^ 1)
 		}
 	}
 
-	const total = (count * 2) - ((race.duration - 1) % 2)
-	return total
+	return 0
 }
 
 function calculateDistance(duration: number, speed: number): number {
